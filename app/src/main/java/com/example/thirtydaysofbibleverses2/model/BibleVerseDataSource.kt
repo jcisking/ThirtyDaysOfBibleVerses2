c5efbbe2014b5670f305bc49cd3242163c58bb9d
jcisking <gocats098@gmail.com>
2024-03-20 15:40:39 -0700

    added BibleVerseRepository

---
+++ app/src/main/java/com/example/thirtydaysofbibleverses2/model/BibleVerseDataSource.kt
@@ -0,0 +1,194 @@
+package com.example.thirtydaysofbibleverses2.model
+
+import com.example.thirtydaysofbibleverses2.R
+
+object BibleVerseRepository {
+    val bibleVerseItems: List<BibleVerseItem> = listOf(
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day1,
+            verseRes = R.string.day_1_verse,
+            passageRes = R.string.day_1_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day2,
+            verseRes = R.string.day_2_verse,
+            passageRes = R.string.day_2_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day3,
+            verseRes = R.string.day_3_verse,
+            passageRes = R.string.day_3_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day4,
+            verseRes = R.string.day_4_verse,
+            passageRes = R.string.day_4_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day5,
+            verseRes = R.string.day_5_verse,
+            passageRes = R.string.day_5_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day6,
+            verseRes = R.string.day_6_verse,
+            passageRes = R.string.day_6_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day7,
+            verseRes = R.string.day_7_verse,
+            passageRes = R.string.day_7_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day8,
+            verseRes = R.string.day_8_verse,
+            passageRes = R.string.day_8_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day9,
+            verseRes = R.string.day_9_verse,
+            passageRes = R.string.day_9_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day10,
+            verseRes = R.string.day_10_verse,
+            passageRes = R.string.day_10_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day11,
+            verseRes = R.string.day_11_verse,
+            passageRes = R.string.day_11_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day12,
+            verseRes = R.string.day_12_verse,
+            passageRes = R.string.day_12_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day13,
+            verseRes = R.string.day_13_verse,
+            passageRes = R.string.day_13_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day14,
+            verseRes = R.string.day_14_verse,
+            passageRes = R.string.day_14_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day15,
+            verseRes = R.string.day_15_verse,
+            passageRes = R.string.day_15_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day16,
+            verseRes = R.string.day_16_verse,
+            passageRes = R.string.day_16_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day17,
+            verseRes = R.string.day_17_verse,
+            passageRes = R.string.day_17_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day18,
+            verseRes = R.string.day_18_verse,
+            passageRes = R.string.day_18_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day19,
+            verseRes = R.string.day_19_verse,
+            passageRes = R.string.day_19_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day20,
+            verseRes = R.string.day_20_verse,
+            passageRes = R.string.day_20_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day21,
+            verseRes = R.string.day_21_verse,
+            passageRes = R.string.day_21_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day22,
+            verseRes = R.string.day_22_verse,
+            passageRes = R.string.day_22_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day23,
+            verseRes = R.string.day_23_verse,
+            passageRes = R.string.day_23_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day24,
+            verseRes = R.string.day_24_verse,
+            passageRes = R.string.day_24_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day25,
+            verseRes = R.string.day_25_verse,
+            passageRes = R.string.day_25_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day26,
+            verseRes = R.string.day_26_verse,
+            passageRes = R.string.day_26_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day27,
+            verseRes = R.string.day_27_verse,
+            passageRes = R.string.day_27_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day28,
+            verseRes = R.string.day_28_verse,
+            passageRes = R.string.day_28_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day28,
+            verseRes = R.string.day_28_verse,
+            passageRes = R.string.day_28_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day29,
+            verseRes = R.string.day_29_verse,
+            passageRes = R.string.day_29_passage
+        ),
+        BibleVerseItem(
+            dayRes = R.string.day,
+            imageRes = R.drawable.day30,
+            verseRes = R.string.day_30_verse,
+            passageRes = R.string.day_30_passage
+        )
+    )
+}
